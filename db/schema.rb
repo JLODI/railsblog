0990a9477e9b39c794534775b9dce8c773bf7679
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-15 21:03:30 -0800

    removed products table, added comments associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_16_011138) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_16_045731) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,9 +18,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_011138) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
+
+  add_foreign_key "comments", "articles"
 end
