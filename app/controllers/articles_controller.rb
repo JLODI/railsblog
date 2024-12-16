d375b427ec834481461092debe7954c8ce5bfd48
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-15 22:04:06 -0800

    added strong params in article create in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,19 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
-  def create
-    @article = Article.new(title: "...", body: "...")
 
+  def create
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -38,7 +41,7 @@ class ArticlesController < ApplicationController
 
     redirect_to root_path, status: :see_other
   end
-  
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
